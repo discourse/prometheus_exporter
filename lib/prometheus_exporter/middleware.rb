e07fd7c1d6cf4c809a0afb933ddbc3bdf2ce8712
Sam <sam.saffron@gmail.com>
2018-01-25 18:13:49 +1100

    Work in progress, support for web metrics

---
+++ lib/prometheus_exporter/middleware.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'prometheus_exporter/instrumentation/method_profiler'
+require 'prometheus_exporter/client'
+
+class PrometheusExporter::Middleware
+  MethodProfiler = PrometheusExporter::Instrumentation::MethodProfiler
+
+  def initialize(app, config = { instrument: true, client: nil })
+    @app = app
+    @client = config[:client] || PrometheusExporter::Client.new(port: 9412)
+
+    if config[:instrument]
+      if defined? Redis::Client
+        MethodProfiler.patch(Redis::Client, [:call, :call_pipeline], :redis)
+      end
+      if defined? PG::Connection
+        MethodProfiler.patch(PG::Connection, [
+          :exec, :async_exec, :exec_prepared, :send_query_prepared, :query
+        ], :sql)
+      end
+    end
+  end
+
+  def call(env)
+    MethodProfiler.start
+    result = @app.call(env)
+    info = MethodProfiler.stop
+    result
+  ensure
+    status = (result && result[0]) || -1
+    params = env["action_dispatch.request.parameters"]
+    action, controller = nil
+    if params
+      action = params["action"]
+      controller = params["controller"]
+    end
+
+    @client.send_json(
+      type: "web",
+      timings: info,
+      action: action,
+      controller: controller,
+      status: status
+    )
+  end
+end
--- lib/prometheus_exporter/server/collector.rb
+++ lib/prometheus_exporter/server/collector.rb
@@ -13,11 +13,60 @@ module PrometheusExporter::Server
     def process(str)
       obj = JSON.parse(str)
       @mutex.synchronize do
-        metric = @metrics[obj["name"]]
-        if !metric
-          metric = register_metric_unsafe(obj)
+        if obj["type"] == "web"
+          observe_web(obj)
+        else
+          metric = @metrics[obj["name"]]
+          if !metric
+            metric = register_metric_unsafe(obj)
+          end
+          metric.observe(obj["value"], obj["keys"])
         end
-        metric.observe(obj["value"], obj["keys"])
+      end
+    end
+
+    def ensure_web_metrics
+      unless @http_requests
+        @metrics["http_requests"] = @http_requests = PrometheusExporter::Metric::Counter.new(
+          "http_requests",
+          "Total HTTP requests from web app"
+        )
+
+        @metrics["http_duration_seconds"] = @http_duration_seconds = PrometheusExporter::Metric::Summary.new(
+          "http_duration_seconds",
+          "Time spent in HTTP reqs in seconds"
+        )
+
+        @metrics["http_redis_duration_seconds"] = @http_redis_duration_seconds = PrometheusExporter::Metric::Summary.new(
+          "http_redis_duration_seconds",
+          "Time spent in HTTP reqs in redis seconds"
+        )
+
+        @metrics["http_sql_duration_seconds"] = @http_sql_duration_seconds = PrometheusExporter::Metric::Summary.new(
+          "http_sql_duration_seconds",
+          "Time spent in HTTP reqs in SQL in seconds"
+        )
+      end
+    end
+
+    def observe_web(obj)
+      ensure_web_metrics
+
+      labels = {
+        controller: obj["controller"] || "other",
+        action: obj["action"] || "other"
+      }
+
+      @http_requests.observe(1, labels.merge(status: obj["status"]))
+
+      if timings = obj["timings"]
+        @http_duration_seconds.observe(timings["total_duration"], labels)
+        if redis = timings["redis"]
+          @http_redis_duration_seconds.observe(redis["duration"], labels)
+        end
+        if sql = timings["sql"]
+          @http_sql_duration_seconds.observe(sql["duration"], labels)
+        end
       end
     end
 
