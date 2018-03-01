da61778f34132ef762c73527f3e1f93c9c016775
Marius Bobin <marius@mbobin.me>
2018-03-01 22:26:01 +0200

    test it can collect delayed job metrics

--- test/server/collector_test.rb
+++ test/server/collector_test.rb
@@ -78,4 +78,36 @@ class PrometheusCollectorTest < Minitest::Test
     assert(text.include?(v8_str), "must include v8 metric")
     assert(text.include?("minor_gc_count"), "must include counters")
   end
+
+  def test_it_can_collect_delayed_job_metrics
+    collector = PrometheusExporter::Server::Collector.new
+    client = PipedClient.new(collector)
+
+    instrument = PrometheusExporter::Instrumentation::DelayedJob.new(client: client)
+
+    job = Minitest::Mock.new
+    job.expect(:handler, "job_class: Class")
+
+    instrument.call(job, nil, "default") do
+      # nothing
+    end
+
+    failed_job = Minitest::Mock.new
+    failed_job.expect(:handler, "job_class: Object")
+
+    begin
+      instrument.call(failed_job, nil, "default") do
+        boom
+      end
+    rescue
+    end
+
+    result = collector.prometheus_metrics_text
+
+    assert(result.include?("delayed_failed_job_count{job_name=\"Object\"} 1"), "has failed job")
+    assert(result.include?("delayed_job_count{job_name=\"Class\"} 1"), "has working job")
+    assert(result.include?("delayed_job_duration_seconds"), "has duration")
+    job.verify
+    failed_job.verify
+  end
 end
