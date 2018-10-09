6bc119336c02dcd2c667b5186b17c5c5181677bc
Terje Larsen <terlar@gmail.com>
2018-10-09 03:13:01 +0200

    Add support for Puma metrics (#34)
    * Add support for Puma metrics
    
    This implementation groups them all together when running several
    workers. One might potentially want to track them separately by putting
    different labels. However I haven't personally had a use for it, so
    currently it is all grouped together.
    
    * Don't collect metric for Puma workers before they have started
    
    * Align puma metric names with Prometheus naming conventions
    
    * Add usage details for puma metrics

---
+++ lib/prometheus_exporter/instrumentation/puma.rb
@@ -0,0 +1,62 @@
+require 'json'
+
+# collects stats from puma
+module PrometheusExporter::Instrumentation
+  class Puma
+    def self.start(client: nil, frequency: 30)
+      puma_collector = new
+      client ||= PrometheusExporter::Client.default
+      Thread.new do
+        while true
+          begin
+            metric = puma_collector.collect
+            client.send_json metric
+          rescue => e
+            STDERR.puts("Prometheus Exporter Failed To Collect Puma Stats #{e}")
+          ensure
+            sleep frequency
+          end
+        end
+      end
+    end
+
+    def collect
+      metric = {}
+      metric[:type] = "puma"
+      collect_puma_stats(metric)
+      metric
+    end
+
+    def collect_puma_stats(metric)
+      stats = JSON.parse(::Puma.stats)
+
+      if stats.key? 'workers'
+        metric[:phase] = stats["phase"]
+        metric[:workers_total] = stats["workers"]
+        metric[:booted_workers_total] = stats["booted_workers"]
+        metric[:old_workers_total] = stats["old_workers"]
+
+        stats["worker_status"].each do |worker|
+          next if worker["last_status"].empty?
+          collect_worker_status(metric, worker["last_status"])
+        end
+      else
+        collect_worker_status(metric, stats)
+      end
+    end
+
+    private
+
+    def collect_worker_status(metric, status)
+      metric[:request_backlog_total] ||= 0
+      metric[:running_threads_total] ||= 0
+      metric[:thread_pool_capacity_total] ||= 0
+      metric[:max_threads_total] ||= 0
+
+      metric[:request_backlog_total] += status["backlog"]
+      metric[:running_threads_total] += status["running"]
+      metric[:thread_pool_capacity_total] += status["pool_capacity"]
+      metric[:max_threads_total] += status["max_threads"]
+    end
+  end
+end
---
+++ lib/prometheus_exporter/server/puma_collector.rb
@@ -0,0 +1,48 @@
+module PrometheusExporter::Server
+  class PumaCollector < TypeCollector
+    PUMA_GAUGES = {
+      workers_total: "Number of puma workers.",
+      booted_workers_total: "Number of puma workers booted.",
+      old_workers_total: "Number of old puma workers.",
+      running_threads_total: "Number of puma threads currently running.",
+      request_backlog_total: "Number of requests waiting to be processed by a puma thread.",
+      thread_pool_capacity_total: "Number of puma threads available at current scale.",
+      max_threads_total: "Number of puma threads at available at max scale.",
+    }
+
+    def initialize
+      @puma_metrics = []
+    end
+
+    def type
+      "puma"
+    end
+
+    def metrics
+      return [] if @puma_metrics.length == 0
+
+      metrics = {}
+
+      @puma_metrics.map do |m|
+        labels = {}
+        if m["phase"]
+          labels.merge(phase: m["phase"])
+        end
+
+        PUMA_GAUGES.map do |k, help|
+          k = k.to_s
+          if v = m[k]
+            g = metrics[k] ||= PrometheusExporter::Metric::Gauge.new("puma_#{k}", help)
+            g.observe(v, labels)
+          end
+        end
+      end
+
+      metrics.values
+    end
+
+    def collect(obj)
+      @puma_metrics << obj
+    end
+  end
+end
