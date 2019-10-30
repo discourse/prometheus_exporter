4a91bba4fcf5fe96932cf667e373565aab3fa8ed
jasquat <2487833+jasquat@users.noreply.github.com>
2019-10-30 02:11:45 -0400

    allow passing opts to Histogram and Summary from client. w/ burnettk (#46)
    * allow passing opts to Histogram and Summary from client. w/ burnettk
    
    * default opts to nil and let collector create the hash when needed and added client tests for it. w/ burnettk

--- lib/prometheus_exporter/client.rb
+++ lib/prometheus_exporter/client.rb
@@ -4,16 +4,16 @@ require 'socket'
 require 'thread'
 
 module PrometheusExporter
-
   class Client
     class RemoteMetric
       attr_reader :name, :type, :help
 
-      def initialize(name:, help:, type:, client:)
+      def initialize(name:, help:, type:, client:, opts: nil)
         @name = name
         @help = help
         @client = client
         @type = type
+        @opts = opts
       end
 
       def standard_values(value, keys, prometheus_exporter_action = nil)
@@ -25,6 +25,7 @@ module PrometheusExporter
           value: value
         }
         values[:prometheus_exporter_action] = prometheus_exporter_action if prometheus_exporter_action
+        values[:opts] = @opts if @opts
         values
       end
 
@@ -39,7 +40,6 @@ module PrometheusExporter
       def decrement(keys = nil, value = 1)
         @client.send_json(standard_values(value, keys, :decrement))
       end
-
     end
 
     def self.default
@@ -83,8 +83,8 @@ module PrometheusExporter
       @custom_labels = custom_labels
     end
 
-    def register(type, name, help)
-      metric = RemoteMetric.new(type: type, name: name, help: help, client: self)
+    def register(type, name, help, opts = nil)
+      metric = RemoteMetric.new(type: type, name: name, help: help, client: self, opts: opts)
       @metrics << metric
       metric
     end
