68640d092a43d3e2d451a69c48a9ac1f421d5295
Brent Dearth <brent.dearth@gmail.com>
2020-12-29 19:27:34 -0700

    backfill specs

--- test/middleware_test.rb
+++ test/middleware_test.rb
@@ -39,35 +39,47 @@ class PrometheusExporterMiddlewareTest < Minitest::Test
     end
   end
 
-  def test_converting_apache_request_start
-    now_microsec = '1234567890123456'
-
-    header 'X-Request-Start', "t=#{now_microsec}"
+  def assert_valid_headers_response(delta = 0.5)
     get '/'
     assert last_response.ok?
-
     refute_nil client.last_send
     refute_nil client.last_send[:queue_time]
-    assert_in_delta 1, client.last_send[:queue_time], 0.05
+    assert_in_delta 1, client.last_send[:queue_time], delta
+  end
+
+  def assert_invalid_headers_response
+    get '/'
+    assert last_response.ok?
+    refute_nil client.last_send
+    assert_nil client.last_send[:queue_time]
+  end
+
+  def test_converting_apache_request_start
+    now_microsec = '1234567890123456'
+    header 'X-Request-Start', "t=#{now_microsec}"
+    assert_valid_headers_response
   end
 
   def test_converting_nginx_request_start
     now = '1234567890.123'
     header 'X-Request-Start', "t=#{now}"
-    get '/'
-    assert last_response.ok?
-
-    refute_nil client.last_send
-    refute_nil client.last_send[:queue_time]
-    assert_in_delta 1, client.last_send[:queue_time], 0.05
+    assert_valid_headers_response
   end
 
-  def test_a_header_in_wrong_format
+  def test_request_start_in_wrong_format
     header 'X-Request-Start', ""
-    get '/'
-    assert last_response.ok?
-
-    refute_nil client.last_send
-    assert_nil client.last_send[:queue_time]
+    assert_invalid_headers_response
   end
+
+  def test_converting_amzn_trace_id_start
+    now = '1234567890'
+    header 'X-Amzn-Trace-Id', "Root=1-#{now.to_i.to_s(16)}-abc123"
+    assert_valid_headers_response
+  end
+
+  def test_amzn_trace_id_in_wrong_format
+    header 'X-Amzn-Trace-Id', ""
+    assert_invalid_headers_response
+  end
+
 end
