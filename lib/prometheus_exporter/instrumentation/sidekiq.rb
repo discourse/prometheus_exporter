discourse/prometheus_exporter

commit 7c59d3cc30bda65dd018fcd923ba35974aa06dc9

DEV: Fix rubocop warnings (#199)

lib/prometheus_exporter/instrumentation/sidekiq.rb
@@ -73,7 +73,7 @@ module PrometheusExporter::Instrumentation
       # of the delayed extensions
       # https://github.com/mperham/sidekiq/blob/master/lib/sidekiq/extensions/class_methods.rb
       begin
-        (target, method_name, _args) = YAML.load(msg['args'].first)
+        (target, method_name, _args) = YAML.safe_load(msg['args'].first)
         if target.class == Class
           "#{target.name}##{method_name}"
         else

bin/prometheus_exporter
@@ -88,7 +88,7 @@ def run
   end
 
   if custom_collector_filename
-    eval File.read(custom_collector_filename), nil, File.expand_path(custom_collector_filename)
+    require File.expand_path(custom_collector_filename)
     found = false
 
     base_klass = PrometheusExporter::Server::CollectorBase
@@ -108,7 +108,7 @@ def run
 
   if custom_type_collectors_filenames.length > 0
     custom_type_collectors_filenames.each do |t|
-      eval File.read(t), nil, File.expand_path(t)
+      require File.expand_path(t)
     end
 
     ObjectSpace.each_object(Class) do |klass|

commit 345a160ffb730e0d3aad29d4e65840a25917ab3f

FIX: Update the CI workflow and fix regression (#198)
* DEV: Update the CI workflow
* DEV: Update mini_racer
* DEV: Don't use appraisal gem in CI (see: https://github.com/thoughtbot/appraisal/pull/174)
* DEV: Don't fail fast
* FIX: A regression in #199

.github/workflows/ci.yml
@@ -2,6 +2,8 @@ name: Test Exporter
 
 on:
   push:
+    branches:
+      - main
   pull_request:
   schedule:
     - cron: '0 0 * * 0' # weekly
@@ -9,41 +11,27 @@ on:
 jobs:
   build:
     runs-on: ubuntu-latest
-    name: Ruby ${{ matrix.ruby }}
+    name: Ruby ${{ matrix.ruby }} AR ${{ matrix.activerecord }}
     strategy:
+      fail-fast: false
       matrix:
-        ruby: ["3.0", "2.7", "2.6"]
+        ruby: ['3.0', '2.7', '2.6']
+        activerecord: ['61', '60']
+    env:
+      BUNDLE_GEMFILE: ${{ github.workspace }}/gemfiles/ar_${{ matrix.activerecord }}.gemfile
+
     steps:
-      - uses: actions/checkout@master
+      - uses: actions/checkout@v2
         with:
           fetch-depth: 1
-      - uses: actions/setup-ruby@v1
+
+      - uses: ruby/setup-ruby@v1
         with:
           ruby-version: ${{ matrix.ruby }}
-      - uses: actions/cache@v2
-        with:
-          path: vendor/bundle
-          key: ${{ runner.os }}-${{ matrix.ruby }}-gems-v2-${{ hashFiles('**/Gemfile.lock') }}
-          restore-keys: |
-            ${{ runner.os }}-${{ matrix.ruby }}-gems-v2-
+          bundler-cache: true
 
-      - name: Downgrade rubygems
-        run: |
-          # for Ruby <= 2.6 , details https://github.com/rubygems/rubygems/issues/3284
-          gem update --system 3.0.8
-        if: ${{ matrix.ruby == '2.6' || matrix.ruby == '2.7' }}
-      - name: Upgrade rubygems
-        run: |
-          gem update --system
-      - name: Setup gems
-        run: |
-          gem install bundler
-          bundle config path vendor/bundle
-          bundle install --jobs 4
-          bundle exec appraisal install
       - name: Rubocop
         run: bundle exec rubocop
-      - name: install gems
-        run: bundle exec appraisal bundle
+
       - name: Run tests
-        run: bundle exec appraisal rake
+        run: bundle exec rake

lib/prometheus_exporter/instrumentation/sidekiq.rb
@@ -73,7 +73,7 @@ module PrometheusExporter::Instrumentation
       # of the delayed extensions
       # https://github.com/mperham/sidekiq/blob/master/lib/sidekiq/extensions/class_methods.rb
       begin
-        (target, method_name, _args) = YAML.safe_load(msg['args'].first)
+        (target, method_name, _args) = YAML.load(msg['args'].first) # rubocop:disable Security/YAMLLoad
         if target.class == Class
           "#{target.name}##{method_name}"
         else

prometheus_exporter.gemspec
@@ -31,7 +31,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 13.0"
   spec.add_development_dependency "minitest", "~> 5.0"
   spec.add_development_dependency "guard", "~> 2.0"
-  spec.add_development_dependency "mini_racer", "~> 0.3.1"
+  spec.add_development_dependency "mini_racer", "~> 0.5.0"
   spec.add_development_dependency "guard-minitest", "~> 2.0"
   spec.add_development_dependency "oj", "~> 3.0"
   spec.add_development_dependency "rack-test", "~> 0.8.3"