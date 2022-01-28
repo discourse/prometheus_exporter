3b0032b206024fd2a196c92ac337ee15fea6bd23
Mathieu Dupuy <deronnax@gmail.com>
2022-01-28 18:40:58 +0100

    DEV: Remove conditional for ruby 2.3 support (#213)

--- lib/prometheus_exporter/metric/base.rb
+++ lib/prometheus_exporter/metric/base.rb
@@ -106,15 +106,8 @@ module PrometheusExporter::Metric
       end
     end
 
-    # when we drop Ruby 2.3 we can drop this
-    if "".respond_to? :match?
-      def needs_escape?(str)
-        str.match?(/[\n"\\]/m)
-      end
-    else
-      def needs_escape?(str)
-        !!str.match(/[\n"\\]/m)
-      end
+    def needs_escape?(str)
+      str.match?(/[\n"\\]/m)
     end
 
   end
