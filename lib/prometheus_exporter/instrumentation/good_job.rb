b29f9fe6fe0bdf8164e808454f5cb5ab9fd054c3
Omar Shammas <omar.shammas@gmail.com>
2024-08-26 16:55:44 -0700

    fix bug, handle when queue is empty
    When we do `group(:queue_name).size` it returns the count by queue
    => {"queue_a"=>3, "queue_d"=>1}
    
    The problem is when a queue is empty it will simply be excluded from the
    results instead of returning count of 0. So the result we want should be
    => {"queue_a"=>3, "queue_b"=>0, "queue_c"=>0, "queue_d"=>1}
    
    Without returning 0, the queue count in prometheus metrics will be the
    last non-zero value meaning we can't auto-scale down the workers.
    
    Bug Fix Refactor

--- lib/prometheus_exporter/instrumentation/good_job.rb
+++ lib/prometheus_exporter/instrumentation/good_job.rb
@@ -3,8 +3,21 @@
 # collects stats from GoodJob
 module PrometheusExporter::Instrumentation
   class GoodJob < PeriodicStats
-    COUNT_BY_QUEUE = ->(collection) { collection.group(:queue_name).size }
-    COUNT_ALL = ->(collection) { collection.size }
+    TotalCounter = Struct.new do
+      def count(relation)
+        relation.size
+      end
+    end
+
+    QueueCounter = Struct.new(:queue_names) do
+      def initialize(queue_names)
+        @empty_queues = queue_names.to_h { |name| [name, 0] }
+      end
+
+      def count(relation)
+        @empty_queues.merge(relation.group(:queue_name).size)
+      end
+    end
 
     def self.start(client: nil, frequency: 30, collect_by_queue: false)
       good_job_collector = new
@@ -18,17 +31,18 @@ module PrometheusExporter::Instrumentation
     end
 
     def collect(by_queue = false)
-      count_method = by_queue ? COUNT_BY_QUEUE : COUNT_ALL
+      counter = by_queue ? QueueCounter.new(::GoodJob::Job.distinct.pluck(:queue_name)) : TotalCounter.new
+
       {
         type: "good_job",
         by_queue: by_queue,
-        scheduled: ::GoodJob::Job.scheduled.yield_self(&count_method),
-        retried: ::GoodJob::Job.retried.yield_self(&count_method),
-        queued: ::GoodJob::Job.queued.yield_self(&count_method),
-        running: ::GoodJob::Job.running.yield_self(&count_method),
-        finished: ::GoodJob::Job.finished.yield_self(&count_method),
-        succeeded: ::GoodJob::Job.succeeded.yield_self(&count_method),
-        discarded: ::GoodJob::Job.discarded.yield_self(&count_method)
+        scheduled: counter.count(::GoodJob::Job.scheduled),
+        retried: counter.count(::GoodJob::Job.retried),
+        queued: counter.count(::GoodJob::Job.queued),
+        running: counter.count(::GoodJob::Job.running),
+        finished: counter.count(::GoodJob::Job.finished),
+        succeeded: counter.count(::GoodJob::Job.succeeded),
+        discarded: counter.count(::GoodJob::Job.discarded)
       }
     end
   end
